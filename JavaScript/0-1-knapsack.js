663b651835f32aec6ed12bfaaad51f29be236607
Kunle Oshiyoye <mgenglder@github.com>
2023-05-13 19:02:52 -0400

    Initial commit containing 0-1 knapsack js implementation

---
+++ JavaScript/0-1-knapsack.js
@@ -0,0 +1,23 @@
+const values = [10, 20, 30, 40];
+const weights = [30, 10, 40, 20];
+const capacity = 40;
+
+function getKnapSack(capacity, n, values, weights) {
+    // Check capacity and items on zero
+    if(capacity === 0 || n === 0) {
+        return 0;
+    }
+
+    // if weight of current element is less than or equal to capacity we can either include or exclude item
+    if(weights[n] <= capacity) {
+        return Math.max(
+            values[n] + getKnapSack(capacity - weights[n], n - 1, values, weights),
+            getKnapSack(capacity, n - 1, values, weights)
+        );
+    }
+
+    // if weight of current element is greater than the capacity we will not include it thus returning just the ignoring part.
+    return getKnapSack(capacity, n - 1, values, weights);
+}
+
+getKnapSack(capacity, values.length - 1, weights);
---
+++ README.md
@@ -0,0 +1,3 @@
+## Algorithms Repo
+---
+A place to store the algorithms I've been practicing. Just for the hecks of it.
